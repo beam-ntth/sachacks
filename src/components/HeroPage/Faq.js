8e2217f13af5f2afe211814d04ee1249a687bc1b
travischantravis <chancheukyin4463@gmail.com>
2020-10-14 00:39:22 -0700

    started legacy teams page

--- src/components/HeroPage/Faq.js
+++ src/components/HeroPage/Faq.js
@@ -3,50 +3,99 @@
  * We take advantage of React by creating a card to display the Q&A
  * Please go to the corresponding file to make changes
  * - FAQCard - src/components/HeroPage/FaqCards/FaqCard.js
- * 
+ *
  * We store all the questions and answers in the Question.js file in this same folder
  */
 
-import React, { useState } from 'react';
-import './Faq.css';
+import React, { useState } from "react";
+import "./Faq.css";
 
 // Components
-import * as Questions from './Questions';
-import Cards from './FaqCards/FaqCard';
+import * as Questions from "./Questions";
+import Cards from "./FAQCards/FaqCard";
 
 // Icons
-import { GoTriangleDown } from 'react-icons/go';
+import { GoTriangleDown } from "react-icons/go";
 
 const Faq = () => {
-
-    const [ open, setOpen ] = useState( { open: true } );
+    const [open, setOpen] = useState({ open: true });
 
     return (
         <React.Fragment>
-            <div className='faq'>
+            <div className="faq">
                 <h1>Frequently Asked Questions</h1>
-                <div className='questions'>
-                    <div className='left-side'>
-                        <Cards qa={ Questions.q1 } />
-                        <Cards qa={ Questions.q2 } />
-                        <Cards qa={ Questions.q3 } />
-                        <Cards qa={ Questions.q4 } />
+                <div className="questions">
+                    <div className="left-side">
+                        <Cards qa={Questions.q1} />
+                        <Cards qa={Questions.q2} />
+                        <Cards qa={Questions.q3} />
+                        <Cards qa={Questions.q4} />
                     </div>
-                    <div className='right-side'>
-                        <div className='question'>
-                            <div className='ques' style={ !open ? { color: '#7BDDDE', transition: '0.5s' } : { color: '#fff', transition: '0.5s' } } onClick={ () => setOpen( !open ) }>
-                                Code of Conduct? <GoTriangleDown size='20px' style={ !open ? { marginLeft: '5px', transform: 'rotate(180deg)', transition: '0.5s', color: '#7BDDDE' } : { marginLeft: '5px', transition: '0.5s', color: '#fff' } } />
+                    <div className="right-side">
+                        <div className="question">
+                            <div
+                                className="ques"
+                                style={
+                                    !open
+                                        ? {
+                                              color: "#7BDDDE",
+                                              transition: "0.5s",
+                                          }
+                                        : { color: "#fff", transition: "0.5s" }
+                                }
+                                onClick={() => setOpen(!open)}
+                            >
+                                Code of Conduct?{" "}
+                                <GoTriangleDown
+                                    size="20px"
+                                    style={
+                                        !open
+                                            ? {
+                                                  marginLeft: "5px",
+                                                  transform: "rotate(180deg)",
+                                                  transition: "0.5s",
+                                                  color: "#7BDDDE",
+                                              }
+                                            : {
+                                                  marginLeft: "5px",
+                                                  transition: "0.5s",
+                                                  color: "#fff",
+                                              }
+                                    }
+                                />
                             </div>
-                            <div className='ans' style={ !open ? { animation: 'slideDown 0.5s ease-in' } : { display: 'none' } }>All participants of our event must follow the MLH Code of Conduct. Click the link below to download a pdf for more information <a id='coc' href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf' target='_blank' rel="noopener noreferrer" >MLH Code of Conduct PDF.</a></div>
-                            <div className='line'></div>
+                            <div
+                                className="ans"
+                                style={
+                                    !open
+                                        ? {
+                                              animation:
+                                                  "slideDown 0.5s ease-in",
+                                          }
+                                        : { display: "none" }
+                                }
+                            >
+                                All participants of our event must follow the
+                                MLH Code of Conduct. Click the link below to
+                                download a pdf for more information{" "}
+                                <a
+                                    id="coc"
+                                    href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    MLH Code of Conduct PDF.
+                                </a>
+                            </div>
+                            <div className="line"></div>
                         </div>
-                        <Cards qa={ Questions.q9 } />
-                        <Cards qa={ Questions.q10 } />
+                        <Cards qa={Questions.q9} />
+                        <Cards qa={Questions.q10} />
                     </div>
                 </div>
             </div>
             {/* <div className='faq-bottom'><img src='/faq-bottom.png' alt='FAQ footer' /></div> */}
-            <div id='wave'></div>
+            <div id="wave"></div>
         </React.Fragment>
     );
 };
--- src/pages/legacy/teams/legacy-teams.js
+++ src/pages/legacy/teams/legacy-teams.js
@@ -1,13 +1,43 @@
-
-import React from 'react';
-import './legacy-teams.css';
+import React from "react";
+import "./legacy-teams.css";
 
 // Add more components here
 
-const LegacyTeams = props => {
-    return (
-        <div className='lg-teams'>
+// /
 
+const LegacyTeams = (props) => {
+    return (
+        <div className="lg-teams">
+            <div className="lg-teams-layout">
+                <div className="lg-teams-year-list">
+                    <p classNmae="lg-teams-year">2020</p>
+                    <p classNmae="lg-teams-year">2019</p>
+                    <p classNmae="lg-teams-year">2018</p>
+                </div>
+                <div className="lg-teams-main-panel">
+                    <h2 className="lg-teams-header">2018 Team</h2>
+
+                    <div className="lg-teams-roles">
+                        <p className="lg-teams-role role-active">Executives</p>
+                        <p className="lg-teams-role">Leads</p>
+                        <p className="lg-teams-role">Associates</p>
+                    </div>
+
+                    <div className="lg-teams-group-photo">
+                        <img
+                            src="/legacy/teams/group-photo-1.png"
+                            alt="Group photo 2018"
+                        />
+                    </div>
+
+                    <div className="lg-teams-member-list">
+                        <div>
+                            <p className="lg-teams-role-header">Executives</p>
+                            {/* Component of one member */}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
