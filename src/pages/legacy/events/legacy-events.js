16793ce5e73ebfbc569c0a0cabd63b126de5436e
Steve <steve.n.hoang90@gmail.com>
2020-12-01 00:34:39 -0800

    update

--- src/pages/legacy/events/legacy-events.js
+++ src/pages/legacy/events/legacy-events.js
@@ -23,7 +23,7 @@ import NewsCards from "../../../components/LegacyPage/events/news-cards/news-car
 // Boilerplate for event section components
 const event1 = {
     num: "1",
-    img: "/legacy/events/sachacks-event-2018.png",
+    img: "/legacy/events/2018 facebook banner.png",
     name: "2018 SACHACKS TEAM",
     header: "SacHacks 1",
     date: "November 17, 2018 - November 18, 2018",
@@ -38,7 +38,7 @@ const event2 = {
     num: "2",
     img: "/legacy/events/1a.jpg",
     name: "2020 SACHACKS TEAM",
-    header: "SacHacks 2",
+    header: "SacHacks 2020",
     date: "February 22, 2020 - February 23, 2020",
     des: `Hosted at Allegiant Innovation Center, the second ever SacHacks hosted 200+ hackers even as news broke of the first confirmed case of COVID-19 in Sacramento County.`,
     btn: "2020 SacHacks",
@@ -61,46 +61,46 @@ const news1 = {
     num: "1",
     img: "/legacy/events/sachacks-news-1.png",
     header: "2018 Sacramento Kings Media Release",
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium ornare felis, quis cursus arcu. Sed ac cursus nibh. ",
-    link: ""
+    summary: "On November 17-18, undergraduate, graduate and industry professionals will gather at McClellan Conference Center for a 24-hour coding competition, entitled SacHacks – Sacramento’s first major intercollegiate hackathon.",
+    link: "https://www.nba.com/kings/news/sacramento-kings-sachacks-and-major-league-hacking-host-sacramentos-first-extensive"
 };
 const news2 = {
     num: "2",
-    img: "",
+    img: "/legacy/events/medium-article-news.jpg",
     header: "2018 News Article",
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium ornare felis, quis cursus arcu. Sed ac cursus nibh. ",
-    link: "https://www.nba.com/kings/news/sacramento-kings-sachacks-and-major-league-hacking-host-sacramentos-first-extensive"
+    summary: "The Sacramento Kings are a primary sponsor of SacHacks, an intercollegiate programming competition that more than 500 students from major universities have registered to participate in next month.",
+    link: "https://www.bizjournals.com/sacramento/news/2018/10/17/kings-sponsor-big-intercollegiate-programming.html"
 };
 const news3 = {
     num: "3",
-    img: "",
+    img: "/legacy/events/sachacks-news-1.png",
     header: "2020 Sacramento Kings Press Release",
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium ornare felis, quis cursus arcu. Sed ac cursus nibh. ",
-    link: ""
+    summary: "On February 22-23, undergraduate, graduate and industry professionals will gather at Allegiant Innovation Center for the Second Annual SacHacks – Sacramento’s major intercollegiate, 24-hour coding and competitive hackathon.",
+    link: "https://www.nba.com/kings/news/sacramento-kings-sachacks-and-major-league-hacking-host-second-annual-hackathon-local-college"
 };
 
 // Boilerplate for Medium articles components
 const medium1 ={
     num: "1",
-    img:"",
+    img:"/legacy/events/2018-medium.jpeg",
     header: "Recap of SacHacks 2018",
-    summary: "",
+    summary: "On November 17–18, 2018 at McClellan Conference Center in Sacramento, California, we had the first year of SacHacks, the first intercollegiate hackathon in the Sacramento region.",
     link: "https://medium.com/sachacks/recap-of-sachacks-2018-5ff4f01184b4"
 };
 
 const medium2 ={
     num: "2",
-    img:"",
+    img:"/legacy/events/2020-medium.jpeg",
     header: "Recap of SacHacks 2020",
-    summary: "",
+    summary: "On February 22–23, 2020 at Allegiant Innovation Center in Sacramento, California, we had the second year of SacHacks, the first intercollegiate hackathon in the Sacramento region.",
     link: "https://medium.com/sachacks/recap-of-sachacks-2020-4a4efff61c88"
 };
 
 const medium3 ={
     num: "3",
-    img:"",
+    img:"/legacy/events/last-medium.png",
     header: "Impacting the Outside World by Thinking Inside the 'BOX' ",
-    summary: "",
+    summary: "An active look into IBM Z Mainframe at SacHacks 2020",
     link: "https://medium.com/sachacks/impacting-the-outside-world-by-thinking-inside-the-box-914a2858e69 "
 };
 
