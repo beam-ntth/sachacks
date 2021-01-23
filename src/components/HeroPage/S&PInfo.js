0ea328de9e88e7914845af02c622df61365b766f
beam-ntth <beam4318.t@gmail.com>
2021-01-23 15:52:21 -0800

    added more logos

--- src/components/HeroPage/S&PInfo.js
+++ src/components/HeroPage/S&PInfo.js
@@ -13,6 +13,16 @@ export const sponsor8 = { img: '/sponsors/taskade-logo.png', alt: 'Taskade', lin
 export const sponsor9 = { img: '/sponsors/balsamiq-logo.png', alt: 'Balsamiq', link: 'https://balsamiq.com/' };
 export const sponsor10 = { img: '/sponsors/wolfram.png', alt: 'Wolfram', link: 'https://www.wolframalpha.com/' };
 export const sponsor11 = { img: '/sponsors/sketch.png', alt: 'Sketch', link: 'https://www.sketch.com/' };
+export const sponsor12 = { img: '/sponsors/Adobexd.png', alt: 'Adobe', link: 'https://adobe.ly/38EJb5H' };
+export const sponsor13 = { img: '/sponsors/HyperX.ai', alt: 'HyperX', link: 'https://www.hyperxgaming.com/unitedstates/us' };
+export const sponsor14 = { img: '/sponsors/sublime-text.png', alt: 'Sublime Text', link: 'https://www.sublimetext.com/' };
+export const sponsor15 = { img: '/sponsors/1Password.png', alt: '1 Password', link: 'https://1password.com/' };
+export const sponsor16 = { img: '/sponsors/bobos.png', alt: 'Bobos', link: 'https://eatbobos.com/' };
+export const sponsor17 = { img: '/sponsors/interview-cake.png', alt: 'Interview Cake', link: 'https://www.interviewcake.com/' };
+export const sponsor18 = { img: '/sponsors/Figma.svg', alt: 'Figma', link: 'https://www.figma.com/' };
+export const sponsor19 = { img: '/sponsors/AoPS.png', alt: 'AoPS', link: 'https://artofproblemsolving.com/' };
+export const sponsor20 = { img: '/sponsors/centered.png', alt: 'Centered.app', link: 'https://www.centered.app/' };
+export const sponsor21 = { img: '/sponsors/jetbrains.png', alt: 'Jetbrains', link: 'https://www.jetbrains.com/' };
 
 // Comment this one in after cleared by MLH ->
 export const partner1 = { img: '/partners/mlh-logo-color.svg', alt: 'MLH Logo', link: 'https://mlh.io/' };
@@ -22,3 +32,6 @@ export const partner4 = { img: '/partners/fcc_eg_logo_transparent.png', alt: 'Fr
 export const partner5 = { img: '/partners/acm.png', alt: 'ACM Logo', link: 'http://losriosccd.acm.org/' };
 export const partner6 = { img: '/partners/SDC.png', alt: 'SDC Logo', link: 'https://www.sacdevcollective.org/' };
 export const partner7 = { img: '/partners/wics.png', alt: 'WICS Logo', link: 'https://www.facebook.com/DavisWICS/' };
+export const partner8 = { img: '/partners/RT.png', alt: 'Region Technology', link: 'https://regionbusiness.org/region-technology/' };
+export const partner9 = { img: '/partners/design-buddies.png', alt: 'Design Buddies', link: 'https://designbuddies.community/' };
+export const partner10 = { img: '/partners/swe.png', alt: 'Society of Women Engineers', link: 'https://swe.org/' };
--- src/components/HeroPage/Sponsors.js
+++ src/components/HeroPage/Sponsors.js
@@ -15,34 +15,51 @@ const Sponsors = () => {
                 <Sponsor img={ Info.sponsor1 } />
             </div>
             <div className='sp-row temp'>
-                <Sponsor img={ Info.sponsor5 } />
                 <Sponsor img={ Info.sponsor3 } />
+                <Sponsor img={ Info.sponsor12 } />
                 <Sponsor img={ Info.sponsor4 } />
+                {/* <Sponsor img={ Info.sponsor13 } /> */}
             </div>
             <div className='sp-row temp'>
                 <Sponsor img={ Info.sponsor2 } />
-                <Sponsor img={ Info.sponsor10 } />
                 <Sponsor img={ Info.sponsor11 } />
-                <Sponsor img={ Info.sponsor6 } />
+                <Sponsor img={ Info.sponsor14 } />
+                <Sponsor img={ Info.sponsor10 } />
             </div>
-            <div className='sp-row temp'>
+            <div className='sp-row small-temp'>
+                <Sponsor img={ Info.sponsor15 } />
+                <Sponsor img={ Info.sponsor16 } />
+                <Sponsor img={ Info.sponsor5 } />
+                <Sponsor img={ Info.sponsor17 } />
+            </div>
+            <div className='sp-row xsmall-temp'>
+                <Sponsor img={ Info.sponsor6 } />
                 <Sponsor img={ Info.sponsor9 } />
+                <Sponsor img={ Info.sponsor18 } />
                 <Sponsor img={ Info.sponsor7 } />
+            </div>
+            <div className='sp-row xsmall-temp'>
+                <Sponsor img={ Info.sponsor19 } />
+                <Sponsor img={ Info.sponsor20 } />
                 <Sponsor img={ Info.sponsor8 } />
+                <Sponsor img={ Info.sponsor21 } />
             </div>
             <h1 id='partner' >Partners</h1>
-            <div className='sp-row' id='first-partner-row'>
+            <div className='sp-row temp'>
                 <Sponsor img={ Info.partner1 } />
-            </div>
-            <div className='sp-row'>
                 <Sponsor img={ Info.partner2 } />
+            </div>
+            <div className='sp-row temp'>
+                <Sponsor img={ Info.partner9 } />
                 <Sponsor img={ Info.partner3 } />
                 <Sponsor img={ Info.partner4 } />
             </div>
-            <div className='sp-row'>
-                <Sponsor img={ Info.partner5 } />
-                <Sponsor img={ Info.partner6 } />
+            <div className='sp-row small-temp'>
                 <Sponsor img={ Info.partner7 } />
+                <Sponsor img={ Info.partner8 } />
+                <Sponsor img={ Info.partner6 } />
+                <Sponsor img={ Info.partner10 } />
+                {/* <Sponsor img={ Info.partner5 } /> */}
             </div>
             <div className='sp-space'></div>
         </div>
