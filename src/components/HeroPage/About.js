ec9fdc24652619b6eef905d49f95625c8d72e1ab
beam-ntth <beam4318.t@gmail.com>
2021-02-25 12:12:04 -0800

    Added team application back

--- src/components/HeroPage/About.js
+++ src/components/HeroPage/About.js
@@ -27,10 +27,10 @@ const About = () => {
                 untapped potential of those in Sac by launching their ideas into
                 action during a 36-hour hackathon.{ " " }
             </p>
-            {/* <div className='map-buttons'>
-                <a href='https://sachacks.typeform.com/to/zGQawsSI' target='_blank' rel='noopener noreferrer'><div className='map-button'>Apply Now</div></a>
-                <a href='https://sachacks.typeform.com/to/SXaHQrRC' target='_blank' rel='noopener noreferrer'><div className='map-button'>Mentor</div></a>
-            </div> */}
+            <div className='map-buttons'>
+                <a href='https://form.typeform.com/to/r2sYTJ' target='_blank' rel='noopener noreferrer'><div className='map-button'>Join Team!</div></a>
+                {/* <a href='https://sachacks.typeform.com/to/SXaHQrRC' target='_blank' rel='noopener noreferrer'><div className='map-button'>Mentor</div></a> */}
+            </div>
             <SocialMedia />
         </div>
     );
