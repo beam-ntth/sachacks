c9c6559f9fd1ecfab5b92cf14aed15c4492f2996
Christopher <ckeokot@gmail.com>
2021-03-23 23:21:51 -0700

    Add Lovelace About and Navbar components

---
+++ src/components/LovelacePage/AdaLovelaceWindow.js
@@ -0,0 +1,16 @@
+/**
+ * This component is the Ada Lovelace Window at the very top of the landing page
+ */
+
+import React from "react";
+import "./About-Lovelace.css";
+
+const AdaLovelaceWindow = () => {
+	return (
+		<div className="ada-lovelace-window">
+
+		</div>
+	);
+};
+
+export default AdaLovelaceWindow;
