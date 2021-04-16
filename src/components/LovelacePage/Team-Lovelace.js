b4989af98e5e4f3d0644941534653976e1b340d7
Raunak Anand <rsanand@ucdavis.edu>
2021-04-16 15:32:12 +0530

    added entire team of lovelace

--- src/components/LovelacePage/Team-Lovelace.js
+++ src/components/LovelacePage/Team-Lovelace.js
@@ -18,7 +18,7 @@ const Team_Lovelace = () => {
 							<h3>President of WiCS</h3>
 						</div>
 						<div className = "person person-2">
-							<a href="https://www.linkedin.com/in/nancy-ma-89890b2/">
+							<a href ="https://www.linkedin.com/in/nancy-ma-89890b2/">
 								<img src="/lovelace2021/Team/nancyma.png" className="square square-2"/>
 							</a>
 							<h2>Nancy Ma</h2>
@@ -32,64 +32,274 @@ const Team_Lovelace = () => {
 							<h3>Design</h3>
 						</div>
 						<div className = "person person-4">
-							<a href="https://www.linkedin.com/in/gennifer-hom/">
-								<img src="/lovelace2021/Team/genniferhom.png" className="square square-4"/>
+							<a href="https://www.linkedin.com/in/ke-lin-9352411b7/">
+								<img src="/lovelace2021/Team/kelin.jpeg" className="square square-4"/>
 							</a>
-							<h2>Gennifer Hom</h2>
+							<h2>Ke Lin</h2>
 							<h3>Design</h3>
 						</div>
 					</div>
 					<div className = "rows row-2">
 						<div className = "person person-5">
-							<a href="https://www.linkedin.com/in/ke-lin-9352411b7/">
-								<img src="/lovelace2021/Team/kelin.jpeg" className="square square-6"/>
+							<a href="https://www.linkedin.com/in/gennifer-hom/">
+								<img src="/lovelace2021/Team/genniferhom.png" className="square square-5"/>
 							</a>
-							<h2>Ke Lin</h2>
+							<h2>Gennifer Hom</h2>
 							<h3>Design</h3>
 						</div>
 						<div className = "person person-6">
 							<a href="https://www.linkedin.com/in/kimberlyltran/">
-								<img src="/lovelace2021/Team/kimberlytran.jpeg" className="square square-7"/>
+								<img src="/lovelace2021/Team/kimberlytran.jpeg" className="square square-6"/>
 							</a>
 							<h2>Kimberly Tran</h2>
 							<h3>Design</h3>
 						</div>
 						<div className = "person person-7">
 							<a href="https://www.linkedin.com/in/rachelyapp/">
-								<img src="/lovelace2021/Team/rachelyap.jpeg" className="square square-8"/>
+								<img src="/lovelace2021/Team/rachelyap.jpeg" className="square square-7"/>
 							</a>
 							<h2>Rachel Yap</h2>
 							<h3>Design</h3>
 						</div>
-					</div>
-					<div className = "rows row-3">
 						<div className = "person person-8">
 							<a href="https://www.linkedin.com/in/shjkim/">
-								<img src="/lovelace2021/Team/sallykim.jpeg" className="square square-9"/>
+								<img src="/lovelace2021/Team/sallykim.jpeg" className="square square-8"/>
 							</a>
 							<h2>Sally Kim</h2>
 							<h3>Design</h3>
 						</div>
+					</div>
+					<div className = "rows row-3">
 						<div className = "person person-9">
+							<a href="https://www.linkedin.com/in/su-tingtan/">
+								<img src="/lovelace2021/Team/su-tingtan.jpeg" className="square square-9"/>
+							</a>
+							<h2>Su-Ting Tan</h2>
+							<h3>Finance</h3>
+						</div>
+						<div className = "person person-10">
 							<a href="https://www.linkedin.com/in/aivy-tran-business/">
 								<img src="/lovelace2021/Team/aivytran.jpeg" className="square square-10"/>
 							</a>
 							<h2>Aivy Tran</h2>
 							<h3>Marketing</h3>
 						</div>
-						<div className = "person person-10">
+						<div className = "person person-11">
 							<a href="https://www.linkedin.com/in/ariellee0310/">
 								<img src="/lovelace2021/Team/ariellee.jpeg" className="square square-11"/>
 							</a>
 							<h2>Ariel Lee</h2>
 							<h3>Marketing</h3>
 						</div>
-						<div className = "person person-11">
+						<div className = "person person-12">
 							<a href="https://www.linkedin.com/in/daniellelei/">
 								<img src="/lovelace2021/Team/daniellelei.jpeg" className="square square-12"/>
 							</a>
 							<h2>Danielle Lei</h2>
 							<h3>Marketing</h3>
+						</div>
+					</div>
+					<div className = "rows row-4">
+						<div className = "person person-13">
+							<a href ="">
+								<img src="/lovelace2021/team-profilepics.svg" className="square square-13"/>
+							</a>
+							<h2>Jenny Yao</h2>
+							<h3>Marketing</h3>
+						</div>
+						<div className = "person person-14">
+							<a href="https://www.linkedin.com/in/jess-i-cali/">
+								<img src="/lovelace2021/Team/jessicali.jpeg" className="square square-14"/>
+							</a>
+							<h2>Jessica Li</h2>
+							<h3>Marketing</h3>
+						</div>
+						<div className = "person person-15">
+							<a href="https://www.linkedin.com/in/kristenchan16/">
+								<img src="/lovelace2021/Team/kristenchan.jpeg" className="square square-15"/>
+							</a>
+							<h2>Kristen Chan</h2>
+							<h3>Marketing</h3>
+						</div>
+						<div className = "person person-16">
+							<a href="https://www.linkedin.com/in/maggie-lee-b6b5ba1b8/">
+								<img src="/lovelace2021/Team/maggielee.jpeg" className="square square-16"/>
+							</a>
+							<h2>Maggie Lee</h2>
+							<h3>Marketing</h3>
+						</div>
+					</div>
+					<div className = "rows row-5">
+						<div className = "person person-17">
+							<a href ="https://www.linkedin.com/in/mary-bangloy/">
+								<img src="/lovelace2021/Team/marybangloy.jpeg" className="square square-17"/>
+							</a>
+							<h2>Mary Bangloy</h2>
+							<h3>Marketing</h3>
+						</div>
+						<div className = "person person-18">
+							<a href="https://www.linkedin.com/in/prashansagoel">
+								<img src="/lovelace2021/Team/prashansagoel.jpeg" className="square square-18"/>
+							</a>
+							<h2>Prashansa Goel</h2>
+							<h3>Marketing</h3>
+						</div>
+						<div className = "person person-19">
+							<a href="https://www.linkedin.com/in/rabiya-oberoi-10250252/">
+								<img src="/lovelace2021/Team/rabyaoberoi.png" className="square square-19"/>
+							</a>
+							<h2>Rabiya Oberoi</h2>
+							<h3>Marketing</h3>
+						</div>
+						<div className = "person person-20">
+							<a href="https://www.linkedin.com/in/tina-sibbal/">
+								<img src="/lovelace2021/Team/tinasibbal.jpeg" className="square square-20"/>
+							</a>
+							<h2>Tina Sibbal</h2>
+							<h3>Marketing</h3>
+						</div>
+					</div>
+					<div className = "rows row-6">
+						<div className = "person person-21">
+							<a href ="https://www.linkedin.com/in/allison-schiffmaier-237b781b4/">
+								<img src="/lovelace2021/Team/allisonschiffmaier.jpeg" className="square square-21"/>
+							</a>
+							<h2>Allison Schiffmaier</h2>
+							<h3>Operations</h3>
+						</div>
+						<div className = "person person-22">
+							<a href="https://www.linkedin.com/in/annette-wong-toi-620957204/">
+								<img src="/lovelace2021/Team/annettewong-toi.jpeg" className="square square-22"/>
+							</a>
+							<h2>Annette Wong-Toi</h2>
+							<h3>Operations</h3>
+						</div>
+						<div className = "person person-23">
+							<a href="https://www.linkedin.com/in/divya-nair-csdesign/">
+								<img src="/lovelace2021/Team/divyanair.jpeg" className="square square-23"/>
+							</a>
+							<h2>Divya Nair</h2>
+							<h3>Operations</h3>
+						</div>
+						<div className = "person person-24">
+							<a href="https://www.linkedin.com/in/jan-truong/">
+								<img src="/lovelace2021/Team/jantruong.png" className="square square-24"/>
+							</a>
+							<h2>Jan Truong</h2>
+							<h3>Operations</h3>
+						</div>
+					</div>
+					<div className = "rows row-7">
+						<div className = "person person-25">
+							<a href ="https://www.linkedin.com/in/julia-alberto-0804/">
+								<img src="/lovelace2021/Team/juliaalberto.png" className="square square-25"/>
+							</a>
+							<h2>Julia Alberto</h2>
+							<h3>Operations</h3>
+						</div>
+						<div className = "person person-26">
+							<a href="https://www.linkedin.com/in/mar-ambriz/">
+								<img src="/lovelace2021/Team/marambriz.jpeg" className="square square-26"/>
+							</a>
+							<h2>Mar Ambriz</h2>
+							<h3>Operations</h3>
+						</div>
+						<div className = "person person-27">
+							<a href="https://www.linkedin.com/in/ngan-nguyen-bao/">
+								<img src="/lovelace2021/Team/ngan-bao.jpeg" className="square square-27"/>
+							</a>
+							<h2>Ngan Bao</h2>
+							<h3>Operations</h3>
+						</div>
+						<div className = "person person-28">
+							<a href="https://www.linkedin.com/in/nishtha-ahir-214669199/">
+								<img src="/lovelace2021/Team/nishthaahir.jpeg" className="square square-28"/>
+							</a>
+							<h2>Nishtha Ahir</h2>
+							<h3>Operations</h3>
+						</div>
+					</div>
+					<div className = "rows row-8">
+						<div className = "person person-29">
+							<a href ="https://www.linkedin.com/in/toni-s-70a987122/">
+								<img src="/lovelace2021/Team/tonischiffmaier.jpeg" className="square square-29"/>
+							</a>
+							<h2>Toni Schiffmaier</h2>
+							<h3>Operations</h3>
+						</div>
+						<div className = "person person-30">
+							<a href="https://www.linkedin.com/in/calla-yang/">
+								<img src="/lovelace2021/Team/callayang.jpeg" className="square square-30"/>
+							</a>
+							<h2>Calla Yang</h2>
+							<h3>PM</h3>
+						</div>
+						<div className = "person person-31">
+							<a href="https://www.linkedin.com/in/minhtumnguyen/">
+								<img src="/lovelace2021/Team/minh-tunguyen.jpeg" className="square square-31"/>
+							</a>
+							<h2>Minh-Tu Nguyen</h2>
+							<h3>PM</h3>
+						</div>
+						<div className = "person person-32">
+							<a href="https://www.linkedin.com/in/anahitap/">
+								<img src="/lovelace2021/Team/anahitapochiraju.jpeg" className="square square-32"/>
+							</a>
+							<h2>Anahita Pochiraju</h2>
+							<h3>WiCS</h3>
+						</div>
+					</div>
+					<div className = "rows row-9">
+						<div className = "person person-33">
+							<a href ="https://www.linkedin.com/in/auboni-poddar-929a1b15b/">
+								<img src="/lovelace2021/Team/aubonipoddar.jpeg" className="square square-33"/>
+							</a>
+							<h2>Auboni Poddar</h2>
+							<h3>WiCS</h3>
+						</div>
+						<div className = "person person-34">
+							<a href="https://www.linkedin.com/in/christinahuangj/">
+								<img src="/lovelace2021/Team/christinahuang.png" className="square square-34"/>
+							</a>
+							<h2>Christina Huang</h2>
+							<h3>WiCS</h3>
+						</div>
+						<div className = "person person-35">
+							<a href="https://www.linkedin.com/in/deepa-marti/">
+								<img src="/lovelace2021/Team/deepamarti.jpeg" className="square square-35"/>
+							</a>
+							<h2>Deepa Marti</h2>
+							<h3>WiCS</h3>
+						</div>
+						<div className = "person person-36">
+							<a href="https://www.linkedin.com/in/divyakarthik/">
+								<img src="/lovelace2021/Team/divyakarthik.jpeg" className="square square-36"/>
+							</a>
+							<h2>Divya Karthik</h2>
+							<h3>WiCS</h3>
+						</div>
+					</div>
+					<div className = "rows row-10">
+						<div className = "person person-37">
+							<a href ="https://www.linkedin.com/in/emmyhuynh/">
+								<img src="/lovelace2021/Team/emmyhuynh.jpeg" className="square square-37"/>
+							</a>
+							<h2>Emmy Huynh</h2>
+							<h3>WiCS</h3>
+						</div>
+						<div className = "person person-38">
+							<a href="https://www.linkedin.com/in/jessica-chen-481967195/">
+								<img src="/lovelace2021/Team/jessicachen.jpeg" className="square square-38"/>
+							</a>
+							<h2>Jessica Chen</h2>
+							<h3>WiCS</h3>
+						</div>
+						<div className = "person person-39">
+							<a href="https://www.linkedin.com/in/ma-eint-poe/">
+								<img src="/lovelace2021/Team/maeintpoe.jpeg" className="square square-39"/>
+							</a>
+							<h2>Ma Eint Poe</h2>
+							<h3>WiCS</h3>
 						</div>
 					</div>
 				</div>
